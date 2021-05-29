0467c88b9980638de6cdc565b1ce0b9c54e99c95
Tor Børve Rasmussen <rasmustor18@gmail.com>
2021-05-29 23:15:56 +0200

    fixed reading from file

--- path_server/include/path_server/fileHandling.h
+++ path_server/include/path_server/fileHandling.h
@@ -7,6 +7,6 @@ namespace file{
 
 void savePath(const nav_msgs::Path& path, std::string filename);
 
-void loadPath(nav_msgs::Path& path, std::string filename);
+void readPath(nav_msgs::Path& path, std::string filename);
 
 }
--- path_server/src/fileHandling.cpp
+++ path_server/src/fileHandling.cpp
@@ -36,10 +36,6 @@
 //         z: 0
 //         w: 1
 
-#define BASEDIR "/home/torb/catkin_ws/src/navigation/path_server/src/"
-
-#define LOAD_CHECK(lhs, rhs) \
-    if(lhs != rhs){ROS_WARN_STREAM("load warning: " << lhs << " != " << rhs);}
 
 #include "path_server/fileHandling.h"
 
@@ -47,109 +43,129 @@
 
 #include <fstream>
 
+    
+#define BASEDIR "/home/torb/catkin_ws/src/navigation/path_server/src/"
+
+#if 1
+#define READ_CHECK(lhs, rhs) \
+    if(lhs != rhs){\
+    ROS_WARN_STREAM("read warning: " << lhs << " != " << rhs << ". " << __FILE__ << ":" << __LINE__);}
+
+#define FILE_CHECK(file) \
+    if(file.fail()){ROS_WARN_STREAM("read warning: file.fail(). " << __FILE__ << ":" << __LINE__);}
+
+#else
+#define READ_CHECK(lhs, rhs) ((void)0)
+
+#define FILE_CHECK(file) ((void)0)
+
+#endif
+
 namespace file{
 
-void readHeader(std::ifstream& inFile, std_msgs::Header& header){
-    std::string word;
-    uint32_t num;
-    double dub;
-    inFile >> word; 
-    LOAD_CHECK(word, "header:");
-    inFile >> word;
-    LOAD_CHECK(word, "seq:");
-    inFile >> header.seq;
-    inFile >> word;
-    LOAD_CHECK(word, "stamp:");
-    inFile >> num;
-    header.stamp.sec = num;
-    inFile.get();
-    inFile >> num;
-    header.stamp.nsec = num;
-    // has to check if frame_id = "" this can cause problems later
-    getline(inFile, word); // read rest of previous line
-    getline(inFile, word); // read frame_id line
-    std::stringstream ss{word};
-    ss >> word;
-    LOAD_CHECK(word, "frame_id:");
-    if (!ss.eof()){
-        ss >> header.frame_id;
+    void readHeader(std::ifstream& inFile, std_msgs::Header& header){
+        std::string word;
+        uint32_t num;
+        double dub;
+        inFile >> word; 
+        READ_CHECK(word, "header:");
+        inFile >> word;
+        READ_CHECK(word, "seq:");
+        inFile >> header.seq;
+        inFile >> word;
+        READ_CHECK(word, "stamp:");
+        inFile >> num;
+        header.stamp.sec = num;
+        inFile.get();
+        inFile >> num;
+        header.stamp.nsec = num;
+        // has to check if frame_id = "" this can cause problems later
+        getline(inFile, word); // read rest of previous line
+        getline(inFile, word); // read frame_id line
+        std::stringstream ss{word};
+        ss >> word;
+        READ_CHECK(word, "frame_id:");
+        if (!ss.eof()){
+            ss >> header.frame_id;
+        }
     }
-}
 
-void readPosition(std::ifstream& inFile, geometry_msgs::Point& point){
-    std::string word;
-    inFile >> word;
-    LOAD_CHECK(word, "position:");
-    inFile >> word;
-    LOAD_CHECK(word, "x:");
-    inFile >> point.x;
-    inFile >> word;
-    LOAD_CHECK(word, "y:");
-    inFile >> point.y;
-    inFile >> word;
-    LOAD_CHECK(word, "z:");
-    inFile >> point.z;
-}
-
-void readOrientation(std::ifstream& inFile, geometry_msgs::Quaternion& quat){
-    std::string word;
-    inFile >> word;
-    LOAD_CHECK(word, "orientation:");
-    inFile >> word;
-    LOAD_CHECK(word, "x:");
-    inFile >> quat.x;
-    inFile >> word;
-    LOAD_CHECK(word, "y:");
-    inFile >> quat.y;
-    inFile >> word;
-    LOAD_CHECK(word, "z:");
-    inFile >> quat.z;
-    inFile >> word;
-    LOAD_CHECK(word, "w:");
-    inFile >> quat.w;
-}
-
-void readPose(std::ifstream& inFile, geometry_msgs::Pose& pose){
-    std::string word;
-    inFile >> word;
-    LOAD_CHECK(word, "pose:");
-    readPosition(inFile, pose.position);
-    readOrientation(inFile, pose.orientation);
-}
-
-void savePath(const nav_msgs::Path& path, std::string filename){
-    std::ofstream outFile{BASEDIR + filename};
-    if (!outFile){
-        throw std::runtime_error{"Could not save file " + filename};
+    void readPosition(std::ifstream& inFile, geometry_msgs::Point& point){
+        std::string word;
+        inFile >> word;
+        READ_CHECK(word, "position:");
+        inFile >> word;
+        READ_CHECK(word, "x:");
+        inFile >> point.x;
+        inFile >> word;
+        READ_CHECK(word, "y:");
+        inFile >> point.y;
+        inFile >> word;
+        READ_CHECK(word, "z:");
+        inFile >> point.z;
     }
-    
-    outFile << path;
-}
 
-void loadPath(nav_msgs::Path& path, std::string filename){
-    std::ifstream inFile{BASEDIR + filename};
-    if (!inFile){
-        throw std::runtime_error{"Could not open file " + filename};
+    void readOrientation(std::ifstream& inFile, geometry_msgs::Quaternion& quat){
+        std::string word;
+        inFile >> word;
+        READ_CHECK(word, "orientation:");
+        inFile >> word;
+        READ_CHECK(word, "x:");
+        inFile >> quat.x;
+        inFile >> word;
+        READ_CHECK(word, "y:");
+        inFile >> quat.y;
+        inFile >> word;
+        READ_CHECK(word, "z:");
+        inFile >> quat.z;
+        inFile >> word;
+        READ_CHECK(word, "w:");
+        inFile >> quat.w;
     }
-    std::string word;
-    double dub;
-    // get header:
-    readHeader(inFile, path.header);
-    inFile >> word;
-    LOAD_CHECK(word, "poses[]");
-    unsigned int poseCount = 0;
-    while(inFile >> word){
-        LOAD_CHECK(word, "poses[" + std::to_string(poseCount) + "]:");
-        geometry_msgs::PoseStamped ps;
-        readHeader(inFile,ps.header);
-        readPose(inFile, ps.pose);
-        path.poses.push_back(ps);
-        poseCount++;
+
+    void readPose(std::ifstream& inFile, geometry_msgs::Pose& pose){
+        std::string word;
+        inFile >> word;
+        READ_CHECK(word, "pose:");
+        readPosition(inFile, pose.position);
+        readOrientation(inFile, pose.orientation);
     }
-    if (inFile.fail()){
-        ROS_ERROR_STREAM("Error loading path from " << filename);
-        throw std::runtime_error{"Error loading path from " + filename};
+
+    void savePath(const nav_msgs::Path& path, std::string filename){
+        std::ofstream outFile{BASEDIR + filename};
+        if (!outFile){
+            throw std::runtime_error{"Could not save file " + filename};
+        }
+        
+        outFile << path;
     }
-}
+
+    void readPath(nav_msgs::Path& path, std::string filename){
+        std::ifstream inFile{BASEDIR + filename};
+        if (!inFile){
+            throw std::runtime_error{"Could not open file " + filename};
+        }
+        std::string word;
+        double dub;
+        // get header:
+        readHeader(inFile, path.header);
+        inFile >> word;
+        READ_CHECK(word, "poses[]");
+        FILE_CHECK(inFile);
+        unsigned int poseCount = 0;
+        while(inFile >> word){
+            READ_CHECK(word, "poses[" + std::to_string(poseCount) + "]:");
+            geometry_msgs::PoseStamped ps;
+            readHeader(inFile,ps.header);
+            readPose(inFile, ps.pose);
+            FILE_CHECK(inFile);
+            path.poses.push_back(ps);
+            poseCount++;
+        }
+        if (inFile.fail() && !inFile.eof()){
+            ROS_ERROR_STREAM("reading error for path");
+            throw std::runtime_error{"reading error for path"};
+        }
+    }
 
 }
